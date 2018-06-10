512206531210b88f5292f7062e8db93af3970429
Sstcan <1980856430@qq.com>
2018-06-10 16:39:06 +0800

    背景修改 1111

--- app/src/main/java/com/example/shishengtao/poss/MainActivity.java
+++ app/src/main/java/com/example/shishengtao/poss/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.shishengtao.poss;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+private TextView textView;
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+        textView=findViewById(R.id.btn_1);
+        button=findViewById(R.id.btn_0);
     }
 }
